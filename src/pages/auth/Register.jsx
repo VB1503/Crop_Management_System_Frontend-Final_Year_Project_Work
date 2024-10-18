c7792cfe2f296aab22e80575650e238b44591f06
vijay <vijayanand.15012003@gmail.com>
2024-10-18 20:15:32 +0530

    upgraded login and register form styles

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -136,84 +136,101 @@ function Register() {
   }, []);
 
   return (
-    <div>
-      <div className='Register-form'>
-        <div  className='reg-card'>
-          <div className='logo-section'>
-            <img src='https://t3.ftcdn.net/jpg/02/12/82/12/240_F_212821250_A7fY72OL8WB7SFJ2gWKhnY0qWCelslis.jpg'></img>
-            <p className='login-title'> Register </p>
+    <div className="flex justify-center items-center h-screen">
+        
+      <div className="max-w-md w-full px-2">
+        <div className="bg-white rounded-md p-6">
+        <div className='flex flex-col items-center'>
+              <img src='https://t3.ftcdn.net/jpg/02/12/82/12/240_F_212821250_A7fY72OL8WB7SFJ2gWKhnY0qWCelslis.jpg' className='w-[40px] h-[40px]'></img>
+            <p className='login-title mb-4'> Register</p>
           </div>
           <form action="" onSubmit={handleSubmit}>
-            <div className='name-form'>
-              <div className='regform'>
-                <input type="text"
-                  className='namefill'
+            <div className="flex flex-col space-y-4">
+              <div className="flex flex-col">
+                <input
+                  type="text"
+                  className="py-2 px-4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
                   name="first_name"
                   value={formdata.first_name}
-                  placeholder='First Name'
-                  onChange={handleOnchange} />
+                  placeholder="First Name"
+                  onChange={handleOnchange}
+                />
               </div>
-              <div className='regform'>
-                <input type="text"
-                  className='namefill'
+              <div className="flex flex-col">
+                <input
+                  type="text"
+                  className="py-2 px-4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
                   name="last_name"
                   value={formdata.last_name}
-                  placeholder='Last Name'
-                  onChange={handleOnchange} />
-              </div>
-            </div>
-            <div className='regform'>
-              <input type="text"
-                className={`inputfill ${errors.email && 'input-invalid'}`}
-                name="email"
-                value={formdata.email}
-                placeholder='E-mail Address'
-                onChange={handleOnchange} />
-              {errors.email && <span className="error">{errors.email}</span>}
-            </div>
-            <div className='regform'>
-              <input type="text"
-                className={`inputfill ${errors.phone_number && 'input-invalid'}`}
-                name="phone_number"
-                value={formdata.phone_number}
-                onChange={handleOnchange}
-                placeholder='Phone Number'
-                maxLength={10}
-                required />
-              {errors.phone_number && <span className="error">{errors.phone_number}</span>}
-            </div>
-            <div className='name-form'>
-              <div className='regform'>
-                <input type={showPassword ? "text" : "password"}
-                  className={`namefill ${errors.password && 'invalid-pass'}`}
-                  name="password"
-                  value={formdata.password}
-                  placeholder='Password'
-                  onChange={handleOnchange} />
-                    {showPassword ? (
-                <FaEyeSlash
-                  className="passwordHide"
-                  onClick={() => setShowPassword(!showPassword)}
+                  placeholder="Last Name"
+                  onChange={handleOnchange}
                 />
-              ) : (
-                <FaEye
-                  className="passwordHide"
-                  onClick={() => setShowPassword(!showPassword)}
-                />
-              )}
-                {errors.password && <span className="error">{errors.password}</span>}
               </div>
-              <div className='regform'>
-                <input type="text"
-                  className={`namefill ${errors.password2 && 'invalid-pass'}`}
-                  name="password2"
-                  value={formdata.password2}
-                  placeholder='Confirm Password'
-                  onChange={handleOnchange} />
-                {errors.password2 && <span className="error">{errors.password2}</span>}
+              <div className="flex flex-col">
+                <input
+                  type="text"
+                  className={`py-2 px-4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 ${errors.email && 'border-red-500'}`}
+                  name="email"
+                  value={formdata.email}
+                  placeholder="E-mail Address"
+                  onChange={handleOnchange}
+                />
+                {errors.email && <span className="text-red-500 text-sm">{errors.email}</span>}
+              </div>
+              <div className="flex flex-col">
+                <input
+                  type="text"
+                  className={`py-2 px-4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 ${errors.phone_number && 'border-red-500'}`}
+                  name="phone_number"
+                  value={formdata.phone_number}
+                  onChange={handleOnchange}
+                  placeholder="Phone Number"
+                  maxLength={10}
+                  required
+                />
+                {errors.phone_number && <span className="text-red-500 text-sm">{errors.phone_number}</span>}
+              </div>
+              <div className="flex flex-col space-y-4">
+                <div className="flex flex-col relative">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    className={`py-2 px-4 border border-gray-300 rounded-md focus:outline-none ${errors.password && 'border-red-500'}`}
+                    name="password"
+                    value={formdata.password}
+                    placeholder="Password"
+                    onChange={handleOnchange}
+                  />
+                  {showPassword ? (
+                    <FaEyeSlash
+                      className="absolute top-2.5 right-3 cursor-pointer"
+                      onClick={() => setShowPassword(!showPassword)}
+                    />
+                  ) : (
+                    <FaEye
+                      className="absolute top-2.5 right-3 cursor-pointer"
+                      onClick={() => setShowPassword(!showPassword)}
+                    />
+                  )}
+                  {errors.password && <span className="text-red-500 text-sm">{errors.password}</span>}
+                </div>
+                <div className="flex flex-col">
+                  <input
+                    type="text"
+                    className={`py-2 px-4 border border-gray-300 rounded-md focus:outline-none ${errors.password2 && 'border-red-500'}`}
+                    name="password2"
+                    value={formdata.password2}
+                    placeholder="Confirm Password"
+                    onChange={handleOnchange}
+                  />
+                  {errors.password2 && <span className="text-red-500 text-sm">{errors.password2}</span>}
+                </div>
               </div>
             </div>
-            <input type="submit" value="Register" className="submitButton" />
+            <input
+              type="submit"
+              value="Register"
+              className="mt-4 w-[100%] py-2 px-4 bg-blue-500 text-white rounded-md cursor-pointer hover:bg-blue-600"
+            />
           </form>
           <div className="popup-container">
             <div className="line"></div>
@@ -221,35 +238,44 @@ function Register() {
             <div className="line"></div>
           </div>
           <div className="google-button">
-            {!username &&
+            {!username && (
               <button
-                className="bg-white text-gray-800 font-bold py-2 px-4 border rounded shadow  focus:outline-none "
-                onClick={openGoogleLoginPage}>
+                className="w-full bg-white text-gray-800 font-bold py-2 px-4 border rounded-3xl shadow focus:outline-none hover:bg-gray-100"
+                onClick={openGoogleLoginPage}
+              >
                 <div className="flex items-center justify-center">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 48 48"
                     width="30px"
-                    height="20px">
+                    height="20px"
+                  >
                     <path
                       fill="#FFC107"
-                      d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z" />
+                      d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+                    />
                     <path
                       fill="#FF3D00"
-                      d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z" />
+                      d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+                    />
                     <path
                       fill="#4CAF50"
-                      d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z" />
+                      d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+                    />
                     <path
                       fill="#1976D2"
-                      d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z" />
+                      d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+                    />
                   </svg>
-                  Sign in with Google
+                  Sign up with Google
                 </div>
-              </button>}
+              </button>
+            )}
           </div>
-          <div className='singup-msg'>
-            <p>Already have an account? <Link to="/login">Login</Link></p>
+          <div className="signup-msg text-center mt-4">
+            <p>
+              Already have an account? <Link to="/login" className="text-blue-500">Login</Link>
+            </p>
           </div>
         </div>
       </div>
